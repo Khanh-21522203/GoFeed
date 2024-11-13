cf3cae8c9899c0d153ec98a7db3b865ccd0dc921
Khanh-21522203 <21522203@gm.uit.edu.vn>
2024-11-14 00:39:42 +0700

    Add

---
+++ cmd/api/posts.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/Khanh-21522203/FeedFlow-Go/internal/store"
+)
+
+type CreatePostPayload struct {
+	Title   string   `json:"title"`
+	Content string   `json:"content"`
+	Tags    []string `json:"tags"`
+}
+
+func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request) {
+	var payload CreatePostPayload
+	if err := readJSON(w, r, payload); err != nil {
+		writeJSONError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	post := &store.Post{
+		Title:   payload.Title,
+		Content: payload.Content,
+		Tags:    payload.Tags,
+		// TODO: Change after auth
+		UserID: 1,
+	}
+
+	ctx := r.Context()
+	if err := app.store.Posts.Create(ctx, post); err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	if err := writeJSON(w, http.StatusCreated, post); err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+}
+
+func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	post, err := app.store.Posts.GetByID(ctx, postID)
+}
