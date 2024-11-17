a1417f561e0918c79171534da16bdee2c3634b08
Khanh-21522203 <21522203@gm.uit.edu.vn>
2024-11-18 00:49:10 +0700

    repository pattern for comment, follow

--- internal/dataaccess/database/comment.go
+++ internal/dataaccess/database/comment.go
@@ -1,10 +1,12 @@
 package database
 
 import (
+	"GoFeed/internal/utils"
 	"context"
 	"time"
 
 	"github.com/doug-martin/goqu/v9"
+	"go.uber.org/zap"
 )
 
 var (
@@ -28,7 +30,7 @@ type Comment struct {
 }
 
 type CommentDataAccessor interface {
-	CreateComment(ctx context.Context, account_id uint64, post_id uint64, content string, created_at time.Time) (uint64, error)
+	CreateComment(ctx context.Context, comment Comment) (uint64, error)
 	GetCommentCountOfPost(ctx context.Context, post_id uint64) (int, error)
 	GetCommentsOfPost(ctx context.Context, post_id uint64) ([]uint64, error)
 	UpdateComment(ctx context.Context, comment Comment) error
@@ -38,31 +40,96 @@ type CommentDataAccessor interface {
 
 type commentDataAccessor struct {
 	database Database
+	logger   *zap.Logger
 }
 
-func NewCommentDataAccessor(database *goqu.Database) CommentDataAccessor {
+func NewCommentDataAccessor(database *goqu.Database, logger *zap.Logger) CommentDataAccessor {
 	return &commentDataAccessor{
 		database: database,
+		logger:   logger,
 	}
 }
 
-func (c commentDataAccessor) CreateComment(ctx context.Context, account_id uint64, post_id uint64, content string, created_at time.Time) (uint64, error) {
-	return 0, nil
+func (c commentDataAccessor) CreateComment(ctx context.Context, comment Comment) (uint64, error) {
+	logger := utils.LoggerWithContext(ctx, c.logger)
+
+	_, err := c.database.
+		Insert(TabNameComments).
+		Rows(goqu.Record{
+			ColNameCommentsID:        comment.ID,
+			ColNameCommentsAccountID: comment.AccountID,
+			ColNameCommentsPostID:    comment.PostID,
+			ColNameCommentsContent:   comment.Content,
+			ColNameCommentsCreatedAt: comment.CreatedAt,
+		}).Executor().Exec()
+
+	if err != nil {
+		logger.With(zap.Error(err)).Error("failed to create comment")
+		return 0, err
+	}
+	return comment.ID, nil
 }
 
 func (c commentDataAccessor) GetCommentCountOfPost(ctx context.Context, post_id uint64) (int, error) {
-	return 0, nil
+	logger := utils.LoggerWithContext(ctx, c.logger)
+
+	var comments []uint64
+	err := c.database.
+		Select(ColNameCommentsPostID).
+		Where(goqu.C(ColNameCommentsPostID).Eq(post_id)).
+		ScanValsContext(ctx, &comments)
+
+	if err != nil {
+		logger.With(zap.Error(err)).Error("failed to get comment count of post")
+		return 0, err
+	}
+	return len(comments), nil
 }
 
 func (c commentDataAccessor) GetCommentsOfPost(ctx context.Context, post_id uint64) ([]uint64, error) {
-	return nil, nil
+	logger := utils.LoggerWithContext(ctx, c.logger)
+
+	var comments []uint64
+	err := c.database.
+		Select(ColNameCommentsPostID).
+		Where(goqu.C(ColNameCommentsPostID).Eq(post_id)).
+		ScanValsContext(ctx, &comments)
+
+	if err != nil {
+		logger.With(zap.Error(err)).Error("failed to get comment count of post")
+		return nil, err
+	}
+	return comments, nil
 }
 
 func (c commentDataAccessor) UpdateComment(ctx context.Context, comment Comment) error {
+	logger := utils.LoggerWithContext(ctx, c.logger)
+
+	_, err := c.database.
+		Update(TabNameComments).
+		Set(comment).
+		Where(goqu.C(ColNameCommentsID).Eq(comment.ID)).
+		Executor().Exec()
+
+	if err != nil {
+		logger.With(zap.Error(err)).Error("failed to update comment")
+		return err
+	}
 	return nil
 }
 
 func (c commentDataAccessor) DeleteComment(ctx context.Context, id uint64) error {
+	logger := utils.LoggerWithContext(ctx, c.logger)
+
+	_, err := c.database.
+		Delete(TabNameComments).
+		Where(goqu.C(ColNameCommentsID).Eq(id)).
+		Executor().Exec()
+
+	if err != nil {
+		logger.With(zap.Error(err)).Error("failed to update comment")
+		return err
+	}
 	return nil
 }
 
--- internal/dataaccess/database/follow.go
+++ internal/dataaccess/database/follow.go
@@ -1,9 +1,13 @@
 package database
 
 import (
+	"GoFeed/internal/utils"
 	"context"
 
 	"github.com/doug-martin/goqu/v9"
+	"go.uber.org/zap"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 var (
@@ -21,46 +25,127 @@ type Follow struct {
 }
 
 type FollowDataAccessor interface {
-	CreateFollow(ctx context.Context, account_id uint64, following_id uint64) error
+	CreateFollow(ctx context.Context, follow Follow) error
 	GetFollowerCountOfAccount(ctx context.Context, account_id uint64) (int, error)
 	GetFollowersOfAccount(ctx context.Context, account_id uint64) ([]uint64, error)
 	GetFollowingCountOfAccount(ctx context.Context, account_id uint64) (int, error)
 	GetFollowingsOfAccount(ctx context.Context, account_id uint64) ([]uint64, error)
-	DeleteFollow(ctx context.Context, account_id uint64, following_id uint64) error
+	DeleteFollow(ctx context.Context, follow Follow) error
 	WithDatabase(database Database) FollowDataAccessor
 }
 
 type followDataAccessor struct {
 	database Database
+	logger   *zap.Logger
 }
 
-func NewFollowDataAccessor(database *goqu.Database) FollowDataAccessor {
+func NewFollowDataAccessor(database *goqu.Database, logger *zap.Logger) FollowDataAccessor {
 	return &followDataAccessor{
 		database: database,
+		logger:   logger,
 	}
 }
 
-func (f followDataAccessor) CreateFollow(ctx context.Context, account_id uint64, following_id uint64) error {
+func (f followDataAccessor) CreateFollow(ctx context.Context, follow Follow) error {
+	logger := utils.LoggerWithContext(ctx, f.logger)
+
+	_, err := f.database.
+		Insert(TabNameFollows).
+		Rows(goqu.Record{
+			ColNameFollowsAccountID:   follow.AccountID,
+			ColNameFollowsFollowingID: follow.FollowingID,
+		}).
+		Executor().Exec()
+
+	if err != nil {
+		logger.With(zap.Error(err)).Error("failed to create follow")
+		return status.Error(codes.Internal, "failed to create follow")
+	}
 	return nil
 }
 
 func (f followDataAccessor) GetFollowerCountOfAccount(ctx context.Context, account_id uint64) (int, error) {
-	return 0, nil
+	logger := utils.LoggerWithContext(ctx, f.logger)
+
+	var followers []uint64
+	err := f.database.
+		Select(ColNameFollowsAccountID).
+		From(TabNameFollows).
+		Where(goqu.C(ColNameFollowsFollowingID).Eq(account_id)).
+		ScanValsContext(ctx, &followers)
+
+	if err != nil {
+		logger.With(zap.Error(err)).Error("failed to get followers of account")
+		return 0, status.Error(codes.Internal, "failed to get followers of account")
+	}
+	return len(followers), nil
 }
 
 func (f followDataAccessor) GetFollowersOfAccount(ctx context.Context, account_id uint64) ([]uint64, error) {
-	return nil, nil
+	logger := utils.LoggerWithContext(ctx, f.logger)
+
+	var followers []uint64
+	err := f.database.
+		Select(ColNameFollowsAccountID).
+		From(TabNameFollows).
+		Where(goqu.C(ColNameFollowsFollowingID).Eq(account_id)).
+		ScanValsContext(ctx, &followers)
+
+	if err != nil {
+		logger.With(zap.Error(err)).Error("failed to get followers of account")
+		return nil, status.Error(codes.Internal, "failed to get followers of account")
+	}
+	return followers, nil
 }
 
 func (f followDataAccessor) GetFollowingCountOfAccount(ctx context.Context, account_id uint64) (int, error) {
-	return 0, nil
+	logger := utils.LoggerWithContext(ctx, f.logger)
+
+	var followings []uint64
+	err := f.database.
+		Select(ColNameFollowsFollowingID).
+		From(TabNameFollows).
+		Where(goqu.C(ColNameFollowsAccountID).Eq(account_id)).
+		ScanValsContext(ctx, &followings)
+
+	if err != nil {
+		logger.With(zap.Error(err)).Error("failed to get followers of account")
+		return 0, status.Error(codes.Internal, "failed to get followers of account")
+	}
+	return len(followings), nil
 }
 
 func (f followDataAccessor) GetFollowingsOfAccount(ctx context.Context, account_id uint64) ([]uint64, error) {
-	return nil, nil
+	logger := utils.LoggerWithContext(ctx, f.logger)
+
+	var followings []uint64
+	err := f.database.
+		Select(ColNameFollowsFollowingID).
+		From(TabNameFollows).
+		Where(goqu.C(ColNameFollowsAccountID).Eq(account_id)).
+		ScanValsContext(ctx, &followings)
+
+	if err != nil {
+		logger.With(zap.Error(err)).Error("failed to get followers of account")
+		return nil, status.Error(codes.Internal, "failed to get followers of account")
+	}
+	return followings, nil
 }
 
-func (f followDataAccessor) DeleteFollow(ctx context.Context, account_id uint64, following_id uint64) error {
+func (f followDataAccessor) DeleteFollow(ctx context.Context, follow Follow) error {
+	logger := utils.LoggerWithContext(ctx, f.logger)
+
+	_, err := f.database.
+		Delete(TabNameFollows).
+		Where(
+			goqu.C(ColNameFollowsAccountID).Eq(follow.AccountID),
+			goqu.C(ColNameFollowsFollowingID).Eq(follow.FollowingID),
+		).Executor().Exec()
+
+	if err != nil {
+		logger.With(zap.Error(err)).Error("failed to delete follow")
+		return status.Error(codes.Internal, "failed to delete follow")
+	}
 	return nil
 }
 
